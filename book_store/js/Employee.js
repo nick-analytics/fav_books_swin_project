6c7a4fd0701841773a19578def13ce94d79e8d2a
nick-analytics <135692850+nick-analytics@users.noreply.github.com>
2026-06-02 16:57:47 +1000

    Delete book_store directory

--- book_store/js/Employee.js
+++
@@ -1,8 +0,0 @@
-//inherites from USer for employee privilages
-
-class Employee extends User{
-    constructor(id, role, firstName, lastName, email, password){
-        super(id, role, firstName, lastName, email, password);
-
-    }
-}
